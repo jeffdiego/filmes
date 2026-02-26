5927de1d0d6776af05411e136f7de75e2e710807
JEFFERSON DIEGO LIMA DA SILVA <jeffersonsilva0425@digitalcollege.internal>
2026-02-26 20:25:54 -0300

    Formatando data de lançamento dos filmes...

--- script.js
+++ script.js
@@ -1,14 +1,81 @@
 fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', {
-method: 'GET',
-headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzJhMWU5NzA1ZmQ5OThlYWRkYjY1ZWY0MzhmZWMyNiIsIm5iZiI6MTc1MjAxODg1NC43MzYsInN1YiI6IjY4NmRhZmE2MjkxMzA3NjU2MzcwM2E1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rJXlKvSxImiQKdosZS6WivzKR7R064r6k2jvDIYgg5c'
-}
+    //method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OGU3MDFkYjNmNTUyZTBhNTFjMDlkNDMxMzdiZDI3MCIsIm5iZiI6MTY4ODczMDA1NC44NzgsInN1YiI6IjY0YTdmOWM2OTY1MjIwMDExZGYwOGU3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YhX8YDb0OF8ovacEzdWjUTSWr0xZLaZOItyxsnzgVMI'
+    }
 })
-.then(function(response) {
-    return response.json();
-})
-.then(function(data) {
-console.log(data.results);
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data.results)
 
-})
+        /* 1 - Pegar a referência da tag onde os card serão inseridos */
+        const container = document.getElementById('featuredMovies');
+        container.innerHTML = '';
+
+        const cardsMovies = data.results.map(function (i) {
+            const card = `
+            <div class="col-6 col-sm-4 col-md-3">
+                <div class="movie-card">
+                    <img src="https://media.themoviedb.org/t/p/w440_and_h660_face/${i.poster_path}"
+                        class="card-img-top" alt="${i.original_title}">
+                    <div class="card-body movie-info">
+                        <div class="movie-title">${i.title}</div>
+                        <div class="movie-date">${formatDate(i.release_date)}</div>
+                        
+                    </div>
+                </div>
+            </div>`
+            console.log(card)
+
+
+
+            // O += 
+
+            container.innerHTML += card;
+        })
+
+        /* 
+        criar o card no formato 
+
+        <div class="col-6 col-sm-4 col-md-3">
+                <div class="movie-card">
+                    <img src="https://media.themoviedb.org/t/p/w440_and_h660_face/pyok1kZJCfyuFapYXzHcy7BLlQa.jpg"
+                        class="card-img-top" alt="...">
+                    <div class="card-body movie-info">
+                        <div class="movie-title">O Hobbit</div>
+                        <div class="movie-date">10/10/2025</div>
+                    </div>
+                </div>
+            </div>
+        */
+        /* Vou percorrer cada item */
+        /* const cardsMovies = data.results.map(function (movie) {
+            const card = document.createElement('div');
+            card.classList.add('col-6', 'col-sm-4', 'col-md-3', 'mb-4');
+
+            card.innerHTML = `
+                <div class="movie-card">
+                    <img src="https://media.themoviedb.org/t/p/w440_and_h660_face/${movie.poster_path}"
+                        class="card-img-top" alt="${movie.title}">
+                    <div class="card-body movie-info">
+                        <div class="movie-title">${movie.title}</div>
+                        <div class="movie-date">${movie.release_date}</div>
+                    </div>
+                </div>
+            `;
+
+            container.appendChild(card);
+        }) */
+
+    })
+    .catch(function (error) {
+        console.error('Error:', error);
+    });
+
+    function formatDate(dateString) {
+        const [year, month, day] = dateString.split('-');
+        return `${day}/${month}/${year}`;
+    }
